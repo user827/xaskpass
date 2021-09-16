061bf3e5047b5750757495e9177029b4c1e9191c
user827 <user827@users.noreply.github.com>
2021-09-16 10:24:34 +0300

    Rerun git describe after a commit

--- build.rs
+++ build.rs
@@ -6,6 +6,11 @@ use std::path::{Path, PathBuf};
 fn get_git_version() -> Result<String, Box<dyn std::error::Error>> {
     if Path::new(".git/HEAD").exists() {
         println!("cargo:rerun-if-changed=.git/HEAD");
+        let r = std::fs::read_to_string(".git/HEAD").unwrap();
+        let mut r = r.split_ascii_whitespace();
+        r.next();
+        let branch = r.next().unwrap();
+        println!("cargo:rerun-if-changed=.git/{}", branch);
     }
     let git_version = std::process::Command::new("git").arg("describe").output()?;
     let mut git_version = String::from_utf8(git_version.stdout)?;
@@ -17,11 +22,12 @@ fn main() {
     let full_version =
         get_git_version().unwrap_or_else(|_| std::env::var("CARGO_PKG_VERSION").unwrap());
 
-    // commenting this out as it would make the life hard for linters
-    //assert!(
-    //    full_version.starts_with(&std::env::var("CARGO_PKG_VERSION").unwrap()),
-    //    "latest git tag does not match the version set in cargo"
-    //);
+    assert!(
+        full_version.starts_with(&std::env::var("CARGO_PKG_VERSION").unwrap()),
+        "latest git tag does not match the version set in cargo: {} vs {}",
+        full_version,
+        std::env::var("CARGO_PKG_VERSION").unwrap()
+    );
 
     println!(
         "cargo:rustc-env=XASKPASS_BUILD_FULL_VERSION={}",
